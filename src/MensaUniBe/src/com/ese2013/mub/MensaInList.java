c47a33c3236245a755d29cb34d4346d8312c288e
Cedric Walker <cedric.walker@students.unibe.ch>
2013-10-12 18:16:53 +0200

    Added a universal MensaObject with builder and first version of the MensaList

---
+++ src/MensaUniBe/src/com/ese2013/mub/MensaInList.java
@@ -0,0 +1,30 @@
+package com.ese2013.mub;
+
+import android.content.Context;
+
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MensaInList extends LinearLayout {
+	String name;
+	Mensa mensa;
+	
+	public MensaInList(Context context, Mensa mensa) {
+		super(context);
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		inflater.inflate(R.layout.mensa_in_list, this);
+		this.mensa = mensa;
+		addDescription(mensa.getAdress());
+		addTitle(mensa.getName());
+	}
+	private void addTitle(String menuTitle) {
+		TextView nameText = (TextView) findViewById(R.id.mensa_name_view);
+		nameText.setText(mensa.getName());
+	}
+	private void addDescription(String menuDesc) {
+		TextView adressText = (TextView) findViewById(R.id.mensa_adress_view);
+		adressText.setText(mensa.getAdress());
+	}
+}
--- src/MensaUniBe/src/com/ese2013/mub/MensaListFragment.java
+++ src/MensaUniBe/src/com/ese2013/mub/MensaListFragment.java
@@ -1,16 +1,52 @@
 package com.ese2013.mub;
 
+import java.util.*;// not sure which data structure yet, for test purpose only
+import com.ese2013.mub.Mensa.MensaBuilder;
+
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
-public class MensaListFragment extends Fragment{
 
-	public MensaListFragment(){
-		
+public class MensaListFragment extends Fragment {
+
+	private static List<Mensa> list; // dummy List, data structure?
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View view = inflater.inflate(R.layout.fragment_mensa_list, container,
+				false);
+
+		LinearLayout layout = (LinearLayout) view
+				.findViewById(R.id.mensa_view_layout);
+
+		List<Mensa> list = getMensas();
+
+		for (Mensa mensa : list) {
+			layout.addView(new MensaInList(container.getContext(), mensa));
+		}
+
+		return view;
 	}
-		
+
+	/**
+	 * TODO: Change implementation, get List from webrequest
+	 * 
+	 * @return
+	 */
+	private static List<Mensa> getMensas() {
+		list = new LinkedList<Mensa>();
+
+		list.add(new MensaBuilder().setName("Mensa Gesellschaftsstrasse")
+				.setAdress("Dummy adress\nadsfasdf\ndsfgafadg").build());
+		list.add(new MensaBuilder().setName("Mensa UniTobler")
+				.setAdress("Dummy adress\nadsfasdf\ndsfgafadg").build());
+		return list;
+	}
+
 }
-
-

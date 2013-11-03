476669bc8e5e4b5c9f84e687d733edbb51840094
Timm Gross <timm.gross@students.unibe.ch>
2013-11-03 11:57:43 +0100

    MapFragment zwischenstand

--- src/MensaUniBe/src/com/ese2013/mub/MapFragment.java
+++ src/MensaUniBe/src/com/ese2013/mub/MapFragment.java
@@ -22,11 +22,13 @@ import android.widget.Toast;
 
 import com.ese2013.mub.model.Mensa;
 import com.ese2013.mub.model.Model;
+import com.ese2013.mub.util.NamedLocation;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -35,53 +37,211 @@ public class MapFragment extends Fragment {
 	protected static final float DETAIL_ZOOM = 17;
 	private GoogleMap map;
 	List<Mensa> mensaList;
+	ArrayList<NamedLocation> mensaLocations = new ArrayList<NamedLocation>();
+	private ArrayList<NamedLocation> spinnerList;
+	private LocationManager locationManager;
+	private NamedLocation currentNamedLocation;
+
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_map, container, false);
-		mensaList = Model.getInstance().getMensas();
+		locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+		Location rawLocation = getLocation();
+		if (rawLocation !=null){
+			currentNamedLocation = new NamedLocation("My Location", rawLocation.getLatitude(), rawLocation.getLongitude());
+		}
+		
+		mensaList = getMensaList();
+		mensaLocations= convertMensasToNamedLocations(mensaList);
+		
 		map = ((SupportMapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+		
 		if (map == null) 
 			return view;
 		
+		drawOnMap();
 		
-		Mensa favouriteMensa = drawMensas();
-		Location location =getLocation();
-		Mensa currentLocation = null;
-		ArrayList<Mensa> spinnerList = new ArrayList<Mensa>();
-		if(location!=null){
-			currentLocation = new Mensa("My Location", location.getLatitude(), location.getLongitude());
-			spinnerList.add(currentLocation);
-			
-		}
-		spinnerList.addAll(mensaList);
-	
-	    ArrayAdapter<Mensa> adapter = new ArrayAdapter<Mensa>(getActivity(),
+		spinnerList = new ArrayList<NamedLocation>();
+		spinnerList.addAll(mensaLocations);
+		addCurrentLocationToSpinner();
+	    ArrayAdapter<NamedLocation> adapter = new ArrayAdapter<NamedLocation>(getActivity(),
 	            android.R.layout.simple_spinner_dropdown_item, spinnerList);
 	    Spinner spinFocus = (Spinner)view.findViewById(R.id.focus_spinner);
 	    spinFocus.setAdapter(adapter);
 	    addListenerOnSpinnerItemSelection(spinFocus);
-	    
-	    
-		if(location!=null){
-            //PLACE THE INITIAL MARKER              
-            drawOnMap(location);
-            int spinnerPosition = adapter.getPosition(currentLocation);
-            Toast.makeText(getActivity(), "spinnerPosition: "+ spinnerPosition, Toast.LENGTH_SHORT).show();
-    	    spinFocus.setSelection(spinnerPosition);
-
-         }else{
-        	 if (favouriteMensa==null){
-        		 int spinnerPosition = adapter.getPosition(mensaList.get(0));
-         	    spinFocus.setSelection(spinnerPosition);
-        	 }else{
-        		int spinnerPosition = adapter.getPosition(favouriteMensa);
-          	    spinFocus.setSelection(spinnerPosition);
-        	 }
-        	
-         }
+	    System.out.println("Zoom on Map - onCreate");
+	    zoomOnContent(adapter, spinFocus);
         
 		return view;
+	}
+
+
+
+	private ArrayList<NamedLocation> convertMensasToNamedLocations(
+			List<Mensa> mensaList2) {
+		ArrayList<NamedLocation> results = new ArrayList<NamedLocation>();
+		for (Mensa m : mensaList2) {
+			results.add(new NamedLocation(m));
+		}
+		return results;
+	}
+
+
+
+	private List<Mensa> getMensaList() {
+		return Model.getInstance().getMensas();
+	}
+
+
+	private void addCurrentLocationToSpinner() {
+		if (currentLocationAvail()){
+			NamedLocation currentNamedLocation = getCurrentNamedLocation();
+			spinnerList.add(currentNamedLocation);
+		}
+	}
+
+
+
+	private void zoomOnContent(ArrayAdapter<NamedLocation> adapter,
+			Spinner spinFocus) {
+		if (currentLocationAvail()){
+			if (favouriteMensaSelected(mensaList)){
+				LatLng currentPoint = new LatLng(getCurrentNamedLocation().getLatitude(), getCurrentNamedLocation().getLongitude());
+				LatLng favPoint = new LatLng(getFavMensaNamedLocations().get(0).getLatitude(), getFavMensaNamedLocations().get(0).getLongitude());
+				LatLngBounds bounds = new LatLngBounds(currentPoint, favPoint);
+//				map.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 0));
+				int spinnerPosition = adapter.getPosition(getCurrentNamedLocation());
+	    	    spinFocus.setSelection(spinnerPosition);
+			}else{
+				int spinnerPosition = adapter.getPosition(getCurrentNamedLocation());
+	    	    spinFocus.setSelection(spinnerPosition);
+			}
+		}else{
+			if (favouriteMensaSelected(mensaList)){
+				System.out.println("Zoom to: "+getFavMensaNamedLocations().get(0).toString());
+				int spinnerPosition = adapter.getPosition(getFavMensaNamedLocations().get(0));
+				System.out.println("spinnerPosition: "+spinnerPosition);
+	    	    spinFocus.setSelection(spinnerPosition);
+			}else{
+				int spinnerPosition = adapter.getPosition(mensaLocations.get(2));
+	    	    spinFocus.setSelection(spinnerPosition);
+			}
+		}
+	}
+
+
+
+	private void drawOnMap() {
+		System.out.println(currentLocationAvail() + " + "+ favouriteMensaSelected(mensaList));
+		if (currentLocationAvail()){
+			NamedLocation currentNamedLocation = getCurrentNamedLocation();
+			drawCurrentLocation(currentNamedLocation);
+			if (favouriteMensaSelected(mensaList)){
+				ArrayList<NamedLocation> favlocations = getFavMensaNamedLocations();
+				drawMensasWithClosest(currentNamedLocation);
+				drawRouteFromTo(currentNamedLocation, favlocations);
+			}else{
+				drawMensas();
+			}
+		}else{
+			drawMensas();
+		}
+	}
+
+	
+
+	private void drawMensasWithClosest(NamedLocation currentNamedLocation) {
+		try {
+			Mensa closest= getClosestMensa(currentNamedLocation);
+		
+		for (Mensa m : mensaList) {
+			LatLng mensaLocation = new LatLng(m.getLatitude(), m.getLongitude());
+			Marker marker;
+			if (m==closest){
+				 marker = map.addMarker(new MarkerOptions()
+				.position(mensaLocation)
+				.snippet("Lat:" + currentNamedLocation.getLatitude() + "Lng:"+ currentNamedLocation.getLongitude())
+				.title(m.getName())
+				.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+			}else{
+				marker = map.addMarker(new MarkerOptions().position(mensaLocation).title(m.getName()));
+			}
+		}
+		
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+
+
+	private void drawCurrentLocation(NamedLocation currentNamedLocation) {
+		map.clear();
+		LatLng currentPosition = new LatLng(currentNamedLocation.getLatitude(),
+				currentNamedLocation.getLongitude());
+		Marker marker = map.addMarker(new MarkerOptions()
+		.position(currentPosition)
+		.snippet("Lat:" + currentNamedLocation.getLatitude() + "Lng:"+ currentNamedLocation.getLongitude())
+		.title("Your Location")
+		.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+	}
+
+	private void drawRouteFromTo(NamedLocation currentNamedLocation,
+			ArrayList<NamedLocation> favlocations) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private NamedLocation getCurrentNamedLocation() {
+		assert currentLocationAvail();
+		return currentNamedLocation;
+	}
+
+	private boolean currentLocationAvail() {
+		System.out.println("locManager enabled: "+locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER));
+		if (currentNamedLocation != null){
+			return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+		}else{
+			return false;
+		}
+		 
+	}
+
+	private ArrayList<NamedLocation> getFavMensaNamedLocations() {
+		ArrayList<Mensa> favMensas = getFavouriteMensas(mensaList);
+		ArrayList<NamedLocation> result = new ArrayList<NamedLocation>();
+		for (Mensa m : favMensas){
+			for (NamedLocation nL : mensaLocations){
+				if (nL.getName() == m.getName()){
+					result.add(nL);
+				}
+			}
+		}
+		return result;
+	}
+
+	private ArrayList<Mensa> getFavouriteMensas(List<Mensa> mensaList2) {
+		assert favouriteMensaSelected(mensaList2) == true;
+		ArrayList<Mensa> result = new ArrayList<Mensa>();
+		for (Mensa m : mensaList) {
+			if (m.isFavorite()){
+				result.add(m);
+			}
+		}
+		return result;
+	}
+
+	private boolean favouriteMensaSelected(List<Mensa> mensaList2) {
+		boolean result = false;
+		for (Mensa m : mensaList) {
+			if (m.isFavorite()){
+				result = true;
+			}
+		}
+		return result;
 	}
 
 	private void addListenerOnSpinnerItemSelection(Spinner spinFocus) {
@@ -90,8 +250,10 @@ public class MapFragment extends Fragment {
 			@Override
 			public void onItemSelected(AdapterView<?> parent, View view,
 					int position, long id) {
-				 Mensa mensa = (Mensa) parent.getItemAtPosition(position);
-				 LatLng selectedLocation = new LatLng(mensa.getLatitude(), mensa.getLongitude());
+				System.out.println(" onItemSelected position: "+position);
+				 NamedLocation namedLoc = (NamedLocation) parent.getItemAtPosition(position);
+				 System.out.println("onItemSelected: "+namedLoc);
+				 LatLng selectedLocation = new LatLng(namedLoc.getLatitude(), namedLoc.getLongitude());
         		 map.moveCamera(CameraUpdateFactory.newLatLngZoom(selectedLocation, DETAIL_ZOOM));
 				
 			}
@@ -105,99 +267,16 @@ public class MapFragment extends Fragment {
 		
 	}
 
-	private void drawOnMap(Location location) {
-		
-		if (location!=null){
-			drawMarker(location);
-			drawMensas(location);
-		}else{
-			drawMensas();
-		}
-		
-	}
 
 
-	private Mensa drawMensas() {
-		Mensa favouriteMensa = null;
+	private void drawMensas() {
 		for (Mensa m : mensaList) {
 			LatLng mensaLocation = new LatLng(m.getLatitude(), m.getLongitude());
-			
-
 			Marker marker = map.addMarker(new MarkerOptions().position(mensaLocation).title(m.getName()));
-			
-			if (m.isFavorite()){
-				favouriteMensa = m;
-			}
 		}
-		return favouriteMensa;
 	}
 
-	private Mensa drawMensas(Location location) {
-		Mensa favouriteMensa = null;
-		try {
-			Mensa closest= getClosestMensa(location);
-		
-		for (Mensa m : mensaList) {
-			LatLng mensaLocation = new LatLng(m.getLatitude(), m.getLongitude());
-			Marker marker;
-			if (m==closest){
-				 marker = map.addMarker(new MarkerOptions()
-				.position(mensaLocation)
-				.snippet("Lat:" + location.getLatitude() + "Lng:"+ location.getLongitude())
-				.title(m.getName())
-				.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
-//				 drawRouteFromTo(m, closest);
-			}else{
-				marker = map.addMarker(new MarkerOptions().position(mensaLocation).title(m.getName()));
-			}
-			if (m.isFavorite()){
-				favouriteMensa = m;
-			}
-		}
-		
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return favouriteMensa;
-	}
-	
-//	private void drawRouteFromTo(Mensa current, Mensa closest) {
-////		LatLng currentP = new LatLng(current.getLatitude(), current.getLongitude());
-////		LatLng closestP = new LatLng(closest.getLatitude(), closest.getLongitude());
-//
-//		
-//		findDirections(current.getLatitude(),
-//				current.getLongitude(),
-//				closest.getLatitude(), closest.getLongitude(), GMapV2Direction.MODE_DRIVING );
-//	}
-	
-//	public void handleGetDirectionsResult(ArrayList<LatLng> directionPoints)
-//	{
-//	    Polyline newPolyline;
-////	    GoogleMap mMap = ((SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map)).getMap(); 
-//	    PolylineOptions rectLine = new PolylineOptions().width(3).color(Color.RED);
-//
-//	    for(int i = 0 ; i < directionPoints.size() ; i++) 
-//	    {          
-//	        rectLine.add(directionPoints.get(i));
-//	    }
-//	    newPolyline = map.addPolyline(rectLine);
-//	}
-//
-//
-//	public void findDirections(double fromPositionDoubleLat, double fromPositionDoubleLong, double toPositionDoubleLat, double toPositionDoubleLong, String mode)
-//	{
-//	    Map<String, String> map = new HashMap<String, String>();
-//	    map.put(GetDirectionsAsyncTask.USER_CURRENT_LAT, String.valueOf(fromPositionDoubleLat));
-//	    map.put(GetDirectionsAsyncTask.USER_CURRENT_LONG, String.valueOf(fromPositionDoubleLong));
-//	    map.put(GetDirectionsAsyncTask.DESTINATION_LAT, String.valueOf(toPositionDoubleLat));
-//	    map.put(GetDirectionsAsyncTask.DESTINATION_LONG, String.valueOf(toPositionDoubleLong));
-//	    map.put(GetDirectionsAsyncTask.DIRECTIONS_MODE, mode);
-//
-//	    GetDirectionsAsyncTask asyncTask = new GetDirectionsAsyncTask(this);
-//	    asyncTask.execute(map); 
-//	}
+
 
 	private Mensa getClosestMensa(Location location) throws Exception {
 		Mensa result = null;
@@ -218,22 +297,8 @@ public class MapFragment extends Fragment {
 		return result;
 	}
 
-
-	private void drawMarker(Location location){
-		map.clear();
-		LatLng currentPosition = new LatLng(location.getLatitude(),
-		location.getLongitude());
-		Marker marker = map.addMarker(new MarkerOptions()
-		.position(currentPosition)
-		.snippet("Lat:" + location.getLatitude() + "Lng:"+ location.getLongitude())
-		.title("Your Location")
-		.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
-//		marker.showInfoWindow();
-		
-	}
-	
 	private Location getLocation() {
-		LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+		
 		// Creating a criteria object to retrieve provider
         Criteria criteria = new Criteria();
 
@@ -246,9 +311,11 @@ public class MapFragment extends Fragment {
         LocationListener locationListener = new LocationListener() {
             public void onLocationChanged(Location location) {
             // redraw the marker when get location update.
-            drawOnMap(location);
-            
+//            Toast.makeText(getActivity(), "Location changed", Toast.LENGTH_SHORT).show();
+            	System.out.println("Draw on Map - onLocationChanged");
+            drawOnMap();
           }
+
 
 			@Override
 			public void onProviderDisabled(String arg0) {
@@ -265,12 +332,17 @@ public class MapFragment extends Fragment {
 			@Override
 			public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
 //				Toast.makeText(getActivity(), "onStatusChanged", Toast.LENGTH_SHORT).show();
-				
+//				drawCurrentLocation(getCurrentNamedLocation());
+//	            drawMensas();
 			}};
           
-          locationManager.requestLocationUpdates(provider, 20000, 0, locationListener);
+          locationManager.requestLocationUpdates(provider, 200000, 100, locationListener);
 		return location;
 	}
+
+
+
+
 	@Override
 	public void onDestroyView() {
 		super.onDestroyView();

365611bad825b7472722dd9d29d9b98d60d39d06
Cedric Walker <cedric.walker@students.unibe.ch>
2013-11-04 17:53:32 +0100

    mapbuttonListener shell

---
+++ src/MensaUniBe/src/com/ese2013/mub/MapButtonListener.java
@@ -0,0 +1,19 @@
+package com.ese2013.mub;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import com.ese2013.mub.model.Mensa;
+
+public class MapButtonListener implements OnClickListener{
+	private Mensa mensa;
+
+	public MapButtonListener(Mensa mensa){
+		this.mensa = mensa;
+	}
+	@Override
+	public void onClick(View v) {
+	
+		
+	}
+}

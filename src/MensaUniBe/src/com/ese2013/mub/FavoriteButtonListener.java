2be1fd86f10f11bbff3ee0317f146573605d2ec6
Cedric Walker <cedric.walker@students.unibe.ch>
2013-11-11 13:30:05 +0100

    Nullpointer in FavoriteButtonListener fix

--- src/MensaUniBe/src/com/ese2013/mub/FavoriteButtonListener.java
+++ src/MensaUniBe/src/com/ese2013/mub/FavoriteButtonListener.java
@@ -28,16 +28,13 @@ public class FavoriteButtonListener implements OnClickListener {
 	@Override
 	public void onClick(View viewIn) {
 		mensa.setIsFavorite(!mensa.isFavorite());
-		// Toast.makeText(, mensa.getName() + "is favorite now is: " +
-		// mensa.isFavorite(), Toast.LENGTH_SHORT).show();
+		
 		if (!mensa.isFavorite()) {
 			button.setImageResource(R.drawable.ic_fav_grey);
-			Toast.makeText(frag.getActivity(), mensa.getName()  + "is added to Favorites!", Toast.LENGTH_SHORT).show();
 			if(isInDailyView && !HomeFragment.getShowAllByDay())
 				frag.refreshFavoriteView();
 		} else{
 			button.setImageResource(R.drawable.ic_fav);
-			Toast.makeText(frag.getActivity(), mensa.getName()  + "is removed from Favorites!", Toast.LENGTH_SHORT).show();
 		}
 	}	
 }

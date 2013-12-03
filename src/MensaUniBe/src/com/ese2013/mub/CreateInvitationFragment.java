b7c57424a19e9fdee02422d1a82a28fd1077f5df
Delio Vicini <delio.vicini@students.unibe.ch>
2013-12-03 18:19:39 +0100

    tmp

---
+++ src/MensaUniBe/src/com/ese2013/mub/CreateInvitationFragment.java
@@ -0,0 +1,19 @@
+package com.ese2013.mub;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CreateInvitationFragment extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+		View view = inflater.inflate(R.layout.fragment_create_invitation, container, false);
+
+		
+		return view;
+	}
+
+}

7448f481551f0f4e09e93751e1fb1bc5408d7dfa
Delio Vicini <delio.vicini@students.unibe.ch>
2013-12-03 13:26:25 +0100

    remove friends function added

--- src/MensaUniBe/src/com/ese2013/mub/util/parseDatabase/OnlineDBHandler.java
+++ src/MensaUniBe/src/com/ese2013/mub/util/parseDatabase/OnlineDBHandler.java
@@ -9,6 +9,7 @@ import com.ese2013.mub.social.CurrentUser;
 import com.ese2013.mub.social.FriendRequest;
 import com.ese2013.mub.social.Invitation;
 import com.ese2013.mub.social.User;
+import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParsePush;
@@ -18,9 +19,9 @@ public class OnlineDBHandler {
 	private static final String INV_USER_RESPONSE = "Response", INV_USER_INVITEE = "Invitee",
 			INV_USER_INVITATION = "Invitation", INVITATION_TIME = "Time", INVITATION_FROM = "From",
 			INVITATION_MENSA = "Mensa", INVITATION_MESSAGE = "Message", INVITATION = "Invitation",
-			INVITATION_USER = "InvitationUser", FRIENDSHIP = "Friendship", USER_NICKNAME = "nickname", USER_EMAIL = "email",
-			USER = "AppUser", USER_1 = "user1", USER_2 = "user2", FRIEND_REQUEST_TO = "To", FRIEND_REQUEST_FROM = "From",
-			FRIEND_REQUEST = "FriendRequest";
+			INVITATION_USER = "InvitationUser", FRIENDSHIP = "Friendship", USER_NICKNAME = "nickname",
+			USER_EMAIL = "email", USER = "AppUser", USER_1 = "user1", USER_2 = "user2", FRIEND_REQUEST_TO = "To",
+			FRIEND_REQUEST_FROM = "From", FRIEND_REQUEST = "FriendRequest";
 
 	public User getUser(User user) throws ParseException {
 		ParseObject u = getUserByMail(user);
@@ -70,6 +71,34 @@ public class OnlineDBHandler {
 		friendship.put(USER_1, ParseObject.createWithoutData(USER, user1.getId()));
 		friendship.put(USER_2, ParseObject.createWithoutData(USER, user2.getId()));
 		friendship.saveEventually();
+	}
+
+	public void removeFriendship(User user1, User user2) {		
+		List<ParseQuery<ParseObject>> or = new ArrayList<ParseQuery<ParseObject>>();
+		ParseObject user1Object = ParseObject.createWithoutData(USER, user1.getId());
+		ParseObject user2Object = ParseObject.createWithoutData(USER, user2.getId());
+		
+		ParseQuery<ParseObject> query1 = ParseQuery.getQuery(FRIENDSHIP);
+		query1.whereEqualTo(USER_1, user1Object);
+		query1.whereEqualTo(USER_2, user2Object);
+		
+		ParseQuery<ParseObject> query2 = ParseQuery.getQuery(FRIENDSHIP);
+		query2.whereEqualTo(USER_2, user1Object);
+		query2.whereEqualTo(USER_1, user2Object);
+		
+		or.add(query1);
+		or.add(query2);
+		ParseQuery<ParseObject> query = ParseQuery.or(or);
+		
+		query.findInBackground(new FindCallback<ParseObject>() {
+			@Override
+			public void done(List<ParseObject> objects, ParseException e) {
+				if (e == null) {
+					for (ParseObject p : objects)
+						p.deleteEventually();
+				}
+			}
+		});
 	}
 
 	public void retrieveFriends(CurrentUser user) throws ParseException {
@@ -150,8 +179,9 @@ public class OnlineDBHandler {
 		for (ParseObject invitation : parseInvitations) {
 			ParseObject parseInv = invitation.getParseObject(INV_USER_INVITATION);
 			ParseObject parseFrom = parseInv.getParseObject(INVITATION_FROM);
-			invitations.add(new Invitation(parseInv.getObjectId(), parseUser(parseFrom), new ArrayList<User>(), parseInv
-					.getString(INVITATION_MESSAGE), parseInv.getInt(INVITATION_MENSA), parseInv.getDate(INVITATION_TIME))
+			invitations.add(new Invitation(parseInv.getObjectId(), parseUser(parseFrom), new ArrayList<User>(),
+					parseInv.getString(INVITATION_MESSAGE), parseInv.getInt(INVITATION_MENSA), parseInv
+							.getDate(INVITATION_TIME))
 
 			);
 		}

9c3e7fd993a0c5bf93e62eefdec4647d993a4d54
cedric <cedric.walker@students.unibe.ch>
2013-12-03 15:36:04 +0100

    changed name to ...Listener

---
+++ src/MensaUniBe/src/com/ese2013/mub/AnswerFriendRequestListener.java
@@ -0,0 +1,22 @@
+package com.ese2013.mub;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import com.ese2013.mub.social.FriendRequest;
+import com.ese2013.mub.social.SocialManager;
+
+public class AnswerFriendRequestListener implements OnClickListener {
+	private FriendRequest friendRequest;
+	private boolean accept;
+	
+	public AnswerFriendRequestListener(FriendRequest friendRequest, boolean accept) {
+		this.friendRequest = friendRequest;
+		this.accept = accept;
+	}
+	@Override
+	public void onClick(View v) {
+		SocialManager.getInstance().answerFriendRequest(friendRequest, accept);
+	}
+
+}
